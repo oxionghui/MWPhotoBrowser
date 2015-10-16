c0c5ca2331d9284e48461f4feaac7195c472524f
jianfei <jianfei@inbilin.com>
2015-10-16 15:56:21 +0800

    调整UI

--- Pod/Classes/MWInbilinCaptionView.m
+++ Pod/Classes/MWInbilinCaptionView.m
@@ -11,7 +11,6 @@
 
 static const CGFloat horizontalPadding = 12;
 static const CGFloat verticalPadding = 10;
-static const CGFloat maxHeight = 110;
 
 @interface MWInbilinCaptionView () {
     UITextView *_textView;
@@ -36,6 +35,13 @@ static const CGFloat maxHeight = 110;
     return self;
 }
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    _textView.frame = CGRectMake(horizontalPadding, verticalPadding, self.bounds.size.width - 2 * horizontalPadding, self.bounds.size.height - 2 * verticalPadding);
+}
+
+
 - (void)setPhoto:(id<MWPhoto>)photo {
     _photo = photo;
     [self setupCaption];
@@ -52,32 +58,49 @@ static const CGFloat maxHeight = 110;
         _textView.showsVerticalScrollIndicator = NO;
         _textView.showsHorizontalScrollIndicator = NO;
         _textView.editable = NO;
-        _textView.textContainerInset = UIEdgeInsetsMake(verticalPadding, horizontalPadding, verticalPadding, horizontalPadding);
+        _textView.textContainerInset = UIEdgeInsetsZero;
+        _textView.textContainer.lineFragmentPadding = 0;
         [self addSubview:_textView];
     }
     if (_photo && [_photo respondsToSelector:@selector(caption)]) {
-        _textView.text = [_photo caption] ? [_photo caption] : @" ";
+        NSString *text = [_photo caption] ? [_photo caption] : @" ";
+        _textView.attributedText = [[NSAttributedString alloc] initWithString:text attributes:[self attrbutes]];
+        _textView.contentOffset = CGPointMake(0, 0);
     }
     
     _textView.textAlignment = NSTextAlignmentLeft;
     
-    if (_textView.text == nil ||
-        _textView.text.length == 0) {
+    if (_textView.attributedText == nil ||
+        _textView.attributedText.length == 0) {
         self.hidden = YES;
     } else {
         self.hidden = NO;
     }
+    [self setNeedsLayout];
+    [self layoutIfNeeded];
 }
 
 // Override -sizeThatFits: and return a CGSize specifying the height of your
 // custom caption view. With width property is ignored and the caption is displayed
 // the full width of the screen
 - (CGSize)sizeThatFits:(CGSize)size {
-    CGSize textSize = [_textView.text boundingRectWithSize:CGSizeMake(self.bounds.size.width-horizontalPadding*2, maxHeight - verticalPadding*2)
-                                                   options:NSStringDrawingUsesLineFragmentOrigin
-                                                attributes:@{NSFontAttributeName: _textView.font}
-                                                   context:nil].size;
-    return CGSizeMake(size.width, textSize.height + verticalPadding * 2);
+    CGFloat maxHeight = _textView.font.lineHeight * 5;
+    CGSize textSize = [_textView.attributedText.string boundingRectWithSize:CGSizeMake(self.bounds.size.width-horizontalPadding*2, MAXFLOAT)
+                                                                    options:NSStringDrawingUsesLineFragmentOrigin
+                                                                 attributes:[self attrbutes]
+                                                                    context:nil].size;
+    CGFloat height = (textSize.height > maxHeight ? maxHeight : textSize.height);
+    return CGSizeMake(size.width, height + 2 * verticalPadding);
+}
+
+- (NSDictionary *)attrbutes {
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = 0.0f;
+    
+    NSDictionary *attributes = @{NSParagraphStyleAttributeName: paragraphStyle,
+                                 NSForegroundColorAttributeName: [UIColor whiteColor],
+                                 NSFontAttributeName: [UIFont systemFontOfSize:14]};
+    return attributes;
 }
 
 @end
